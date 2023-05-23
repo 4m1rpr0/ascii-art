4e039710b622c24dd0a9194b4843f6841e89829f
ali sadeghy <chatroom23456@gmail.com>
2023-05-23 04:54:08 -0700

    For correct implementation, we changed the parameter from 0.5 to 1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ while True:
     _, frame = video.read()
     gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("camera", frame)
-    if cv2.waitKey(0.5) == 113:
+    if cv2.waitKey(1) == 113:
         break
 video.release()
 cv2.destroyAllWindows()
