2569724741a2286e8cbff77ab3c34716033571e5
ali sadeghy <chatroom23456@gmail.com>
2023-05-23 05:55:28 -0700

    To speed up the program, we make the image smaller

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ chars = ["█", "▓", "▒", "░", "▌", "@", "#", "$", "%", "▲", "▼", "�
 video = cv2.VideoCapture(0)
 while True:
     _, frame = video.read()
-    gray_frames = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frames_large = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frames_small = cv2.resize(gray_frames_large, (112, 40), interpolation = cv2.INTER_AREA)
     cv2.imshow("camera", frame)
     frame_char = ""
-    for frame in gray_frames:
+    for frame in gray_frames_small:
         counter = 0
         for pixel in frame//10:
             counter += 1
