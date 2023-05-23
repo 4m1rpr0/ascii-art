0eafae550b57ac50f2a8baf74a2fe8a5aad5784d
ali sadeghy <chatroom23456@gmail.com>
2023-05-23 04:48:57 -0700

    With the cvtcolor function, we converted the frames from bgr to grayscale

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 video = cv2.VideoCapture(0)
 while True:
     _, frame = video.read()
+    gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("camera", frame)
     if cv2.waitKey(0.5) == 113:
         break
